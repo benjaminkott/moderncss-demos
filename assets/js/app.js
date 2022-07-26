fe564b753036791d3df4dca6ccfa2dea0b672bec
Benjamin Kott <benjamin.kott@outlook.com>
2022-07-26 12:51:38 +0200

    [TASK] Add range slider to control width

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,7 @@
 import '../css/app.scss';
+
+const preview = document.querySelector('#demo-preview');
+const previewControl = document.querySelector('#demo-preview-control');
+previewControl.addEventListener('input', (event) => {
+    preview.style.width = event.target.value + '%';
+});
