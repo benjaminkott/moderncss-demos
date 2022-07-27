3ed9391098783ba319bee516df34af12372b6ba6
Benjamin Kott <benjamin.kott@outlook.com>
2022-07-27 14:48:09 +0200

    [TASK] Add inner scaling handler

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,8 @@ const previewControl = document.querySelector('#demo-preview-control');
 previewControl.addEventListener('input', (event) => {
     preview.style.width = event.target.value + '%';
 });
+
+const previewInnerControl = document.querySelector('#demo-preview-inner-control');
+previewInnerControl.addEventListener('input', (event) => {
+    preview.contentDocument.querySelector('#demo-inner').style.width = event.target.value + '%';
+});
