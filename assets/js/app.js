0a7d32a715d99d2fe027bd71ed4a43577f8ceb8d
Benjamin Kott <benjamin.kott@outlook.com>
2024-07-24 11:44:13 +0200

    [TASK] Store last split layout

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,19 +3,62 @@ import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 
 const preview = document.getElementById('demo-preview');
 const previewControl = document.getElementById('demo-preview-control');
-previewControl.addEventListener('input', (event) => {
+previewControl?.addEventListener('input', (event) => {
     preview.style.width = event.target.value + '%';
 });
 
 const previewInnerControl = document.getElementById('demo-preview-inner-control');
-previewInnerControl.addEventListener('input', (event) => {
+previewInnerControl?.addEventListener('input', (event) => {
     preview.contentDocument.querySelector('#demo-inner').style.width = event.target.value + '%';
+});
+
+// Split View
+function applySplitViewConfig(config) {
+    if (config === null) {
+        return;
+    }
+    if (config.editorSize === undefined || config.previewSize === undefined) {
+        return;
+    }
+    document.querySelectorAll('button[data-preview-split-editor]').forEach((element) => {
+        element.classList.remove('active');
+    });
+    const button = document.querySelector('button[data-preview-split-editor="'+ config.editorSize + '"][data-preview-split-preview="'+ config.previewSize + '"]');
+    button?.classList.add('active');
+
+    const root = document.getElementById('demo-root');
+    root?.style.setProperty('--columns-editor', config.editorSize);
+    root?.style.setProperty('--columns-preview', config.previewSize);
+    localStorage.setItem('splitViewConfig', JSON.stringify(config));
+}
+
+applySplitViewConfig(JSON.parse(localStorage.getItem("splitViewConfig") ?? '{}'));
+
+document.querySelectorAll('button[data-preview-split-editor]').forEach((button) => {
+    button.addEventListener('click', (event) => {
+        applySplitViewConfig({
+            editorSize: event.target.dataset.previewSplitEditor,
+            previewSize: event.target.dataset.previewSplitPreview
+        });
+    });
+});
+
+// Editors
+document.querySelectorAll('button[data-editor-modify-fontsize]').forEach((button) => {
+    button.addEventListener('click', (event) => {
+        window.previeweditors.forEach(editor => {
+            let options = {
+                fontSize: editor.getRawOptions().fontSize + parseInt(event.target.dataset.editorModifyFontsize)
+            }
+            editor.updateOptions(options);
+        });
+    });
 });
 
 window.previeweditors = [];
 document.querySelectorAll('[data-editor="true"]').forEach((element) => {
     // Tabs
-    const button = element.parentElement.querySelector('button[data-section]').addEventListener('click', (event) => {
+    element.parentElement.querySelector('button[data-section]').addEventListener('click', (event) => {
         event.target.parentElement.parentElement.querySelectorAll('.demo-editor-section').forEach((section) => {
             section.classList.remove('active');
         });
@@ -44,29 +87,7 @@ document.querySelectorAll('[data-editor="true"]').forEach((element) => {
     window.previeweditors.push(editor);
 });
 
-document.querySelectorAll('button[data-preview-split-editor]').forEach((button) => {
-    button.addEventListener('click', (event) => {
-        document.querySelectorAll('button[data-preview-split-editor]').forEach((element) => {
-            element.classList.remove('active');
-        });
-        button.classList.add('active');
-
-        const editorWidth = event.target.dataset.previewSplitEditor;
-        const previewWidth = event.target.dataset.previewSplitPreview;
-        const root = document.getElementById('demo-root');
-
-        root.style.setProperty('--columns-editor', editorWidth);
-        root.style.setProperty('--columns-preview', previewWidth);
-    });
-});
-
-document.querySelectorAll('button[data-editor-modify-fontsize]').forEach((button) => {
-    button.addEventListener('click', (event) => {
-        window.previeweditors.forEach(editor => {
-            let options = {
-                fontSize: editor.getRawOptions().fontSize + parseInt(event.target.dataset.editorModifyFontsize)
-            }
-            editor.updateOptions(options);
-        });
-    });
-});
+const loader = document.querySelector('.demo-loader');
+if (loader) {
+    loader.style.display = 'none';
+}
