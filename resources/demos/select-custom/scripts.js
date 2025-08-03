3ade60d1bd2e65e6c9511baa926a1b1f08b2b1aa
Benjamin Kott <benjamin.kott@outlook.com>
2025-08-03 21:09:46 +0200

    [TASK] Extend custom select

--- resources/demos/select-custom/scripts.js
+++ resources/demos/select-custom/scripts.js
@@ -2,4 +2,13 @@ document.addEventListener('DOMContentLoaded', function() {
     if (!CSS.supports('appearance', 'base-select')) {
         window.demotools.notsupported('This browser does not support <strong>appearance: base-select</strong>.');
     }
+    if (!CSS.supports('scrollbar-color', 'black black')) {
+        window.demotools.notsupported('This browser does not support <strong>scrollbar-color</strong>.');
+    }
+    if (typeof CSSStartingStyleRule !== 'function') {
+        window.demotools.notsupported('This browser does not support <strong>@starting-style</strong>.');
+    }
+    if (!CSS.supports('transition-behavior', 'allow-discrete')) {
+        window.demotools.notsupported('This browser does not support <strong>transition-behavior: allow-discrete</strong>.');
+    }
 });
